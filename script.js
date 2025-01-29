d7001949102b9afbd5ff111448daef128eeee4af
biniiiii <binisha14parajuli@gmail.com>
2025-01-29 22:24:59 +0545

    commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const inputField = document.querySelector('input');
+const keyboardButtons = document.querySelectorAll('.keyboard button');
+
+keyboardButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const buttonText = button.textContent;
+
+        if (buttonText === "Backspace") {
+            inputField.value = inputField.value.slice(0, -1);
+        } else if (buttonText === "Enter") {
+            inputField.value += '\n';
+        } else if (buttonText === "Space") {
+            inputField.value += ' ';
+        } else if (buttonText === "Shift" || buttonText === "Alt" || buttonText === "Alt Gr" || buttonText === "Fn" || buttonText === "Ctrl" || buttonText === "CapsLock" || buttonText === "Win" || buttonText === "Tab") {
+            return;
+        }
+        else {
+            inputField.value += buttonText;
+        }
+    });
+});
